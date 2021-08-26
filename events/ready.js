735d93bb02a7d52406c495698c8fb8bff200a078
aiden <83490070+barelyaiden@users.noreply.github.com>
2021-08-26 21:00:01 +0400

    Initial commit.

---
+++ events/ready.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'ready',
+    once: true,
+    execute(client) {
+        console.log(`Successfully logged in as ${client.user.tag} (${client.user.id})`);
+        client.user.setPresence({ activities: [{ name: 'Sonic and the Black Knight', type: 'PLAYING' }], status: 'dnd' });
+    },
+};
